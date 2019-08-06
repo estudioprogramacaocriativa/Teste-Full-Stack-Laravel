245b688eac7ebcdee6eefda3ebd41f4bdace7198
Samuel dos Santos <contato@programacaocriativa.com.br>
2019-08-05 22:02:52 -0300

    implement filter posts by status

---
+++ resources/js/plugins/search.js
@@ -0,0 +1,44 @@
+import api from './../services/axios'
+
+const filters = () => {
+    let status
+    let reload_data = document.querySelector('.reload-data')
+    let el = document.querySelectorAll('.filters nav li')
+    let el_selector = Array.from(el)
+    let loading_target = document.querySelector('.loading')
+    let container_hide_target = document.querySelector('.hide-when-loading')
+
+    if(el_selector) {
+        el_selector.forEach((item) => {
+            item.addEventListener('click', () => {
+                if (!item.classList.contains('active')){
+                    el_selector.filter(el => toggleClass(el, 'remove', 'active'))
+                    loading_target.style.display = 'block'
+                    container_hide_target.style.display = 'none'
+                    status = item.dataset.filter
+
+                    toggleClass(item, 'add', 'active')
+
+                    api.get(`posts/filter/${status}`)
+                        .then((resp) => reload_data.innerHTML = resp.data.data)
+                        .then(() => {
+                            loading_target.style.display = 'none'
+                            container_hide_target.style.display = 'block'
+                        })
+                        .catch(() => {})
+                }
+            })
+        })
+    }
+}
+
+const toggleClass = (el, action, class_) => {
+    if(el !== undefined) {
+        el.classList[action](class_)
+    }
+}
+
+export default {
+    filters,
+    toggleClass
+}
